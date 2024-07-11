284809c2b450ed919b025450ef6454bf59ef8f51
Christiaan Herrewijn <c.herrewijn@gmail.com>
2024-07-11 22:53:44 +0200

    update MariaDB docker config (with ENTRYPOINT)

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
-
-
 echo "mariaDB setup script!";
 
-# service mariadb start;  ## start + stop not the best route, do mysql_install_db instead
-# see: https://mariadb.com/kb/en/mysql_install_db/
-
-
-## TODO: create initial tables
-
-# service mariadb stop;
-
 # only do this if DB does not yet exist!
-mariadb-install-db
+# e.g. folder: /var/lib/mysql/inception exists!
 
-# start the DB service, see: https://man7.org/linux/man-pages/man1/mysqld_safe.1.html
-mariadbd-safe;
+# needed ??
+# mariadb-install-db
 
-# only do this if DB does not yet exist!
-
-# Not sure if needed!? -> run before the service is running
-#    mariadb-install-db
-
-# only do this if DB does not yet exist! --> note somehow not created!!
+service mariadb start
 mariadb < /tools/createdb.sql
+service mariadb stop
 
-#mariadb -p myPassword
-
-
-
-
-# follwing steps from: https://mariadb.com/kb/en/a-mariadb-primer/
-
-
-# log in to datab
-# CREATE DATABASE IF NOT EXISTS test;
+exec mariadbd-safe
