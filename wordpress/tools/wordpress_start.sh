100b67bb5aa77fb9d08b84e1bbc885585ef38360
Christiaan Herrewijn <c.herrewijn@gmail.com>
2024-07-19 20:08:58 +0200

    added docker compose (wip)

--- wordpress/tools/wordpress_start.sh
+++ wordpress/tools/wordpress_start.sh
@@ -13,7 +13,8 @@ if [ ! -f /var/www/html/inception/wp-config.php ]; then
     mkdir -p /var/www/html/inception
     cd /var/www/html/inception
     wp core download --allow-root
-    wp config create --allow-root --dbname=inception --dbuser=user1 --dbpass=password1 --dbhost=localhost:3306
+    # wp config create --allow-root --dbname=inception --dbuser=user1 --dbpass=password1 --dbhost=localhost:3306
+    wp config create --allow-root --dbname=inception --dbuser=user1 --dbpass=password1 --dbhost=mariadb
     wp core install --allow-root --url=cherrewi.42.fr --title="Inception_WP" --admin_user=wp_admin --admin_password=pass1 --admin_email=cherrewi@student.codam.nl
     wp user create wp_user user1@student.codam.nl --allow-root --role=author --user_pass=pass2
 fi
